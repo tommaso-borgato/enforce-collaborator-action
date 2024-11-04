46ee5e9881cde2f4c2d43a199a95a75bf1636ab0
Tommaso Borgato <tborgato@redhat.com>
2024-11-04 12:28:18 +0100

    This action enforces the current usert to be a repo collaborator

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# enforce-collaborator-action
+# GitHub Action for enforcing repo maintainer
+
+Thsi action enforces the current usert to be a repo maintainer;
+
+## Setup
+
+```bash
+npm init -y
+npm install @actions/core
+npm install @actions/github
+```
+
+## Compile
+
+```bash
+npm i -g @vercel/ncc
+ncc build index.js --license licenses.txt
+```
+
+See https://docs.github.com/en/actions/sharing-automations/creating-actions/creating-a-javascript-action
+
+
+## Quick iteration
+
+```bash
+ncc build index.js --license licenses.txt
+git add -A
+git commit -m "mmm"
+git push origin main --force
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+try {    
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
+    console.log(`Github github.context.payload.repository.full_name: ${github.context.payload.repository.full_name}`);
+    const OWNER = github.context.payload.repository.full_name.split('/')[0];
+    const REPOSITORY = github.context.payload.repository.full_name.split('/')[1];
+
+    // token
+    const github_token = core.getInput('github_token', { required: true });
+
+    // logged in user 
+    const login = github.context.payload.sender.login;
+    console.log(`github.context.payload.sender.login: ${github.context.payload.sender.login}`);
+
+    // collaborators for the current repository
+    const octokit = new github.getOctokit(github_token);
+    const collaborators = await octokit.request('GET /repos/{owner}/{repo}/collaborators', {
+        owner: OWNER,
+        repo: REPOSITORY,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+    console.log(`collaborators: ${JSON.stringify(collaborators, undefined, 2)}`);
+
+    var isCollaborator = false;
+    collaborators.data.forEach(element => {
+        if (element.login == login) {
+            isCollaborator = true;
+        }
+    });
+    console.log(`Is Collaborator: ${isCollaborator}`);
+    core.setOutput("is-maintainer", isCollaborator);
+} catch (error) {
+    core.setOutput("is-maintainer", false);
+    core.setFailed(error.message);
+}
